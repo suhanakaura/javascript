92fcd99ac5a553f5e7986c8700131848781200ba
Suhana Kaura <123460149+suhanakaura@users.noreply.github.com>
2024-03-06 10:25:43 +0000

    opn

---
+++ 01_basic/operation.js
@@ -0,0 +1,30 @@
+let val = 3;
+let negval = -val
+// console.log(negval)
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1+str2 //concantenation
+// console.log(str3)
+
+// console.log(3 + "3")
+// console.log("3" + 3)
+// console.log("3" + 3 + 3) //333
+// console.log(3 + 3 + "3") //63
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+// console.log(true+)//error
+
+let count = 1
+count++
+++count
+console.log(count)
