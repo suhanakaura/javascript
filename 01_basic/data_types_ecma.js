deac945e2593f7cf335c55bd3487ff6192dcbae2
Suhana Kaura <123460149+suhanakaura@users.noreply.github.com>
2024-03-06 08:05:25 +0000

    data types

---
+++ 01_basic/data_types_ecma.js
@@ -0,0 +1,27 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3) //don't work as we are inside node (we are using nodejs not browser)
+// js engine is binded in brower
+
+// data types
+let name = "hello" //string
+let age = 12 //integer
+let istrue = true //boolean
+
+// primitive data types:
+// number => range(2 to power 53)
+// bigint 
+// string => " "
+// boolean => true/false
+// null => standalone value (representation of an empty value)
+// undefined => value not assigned
+// symbol => find the uniqueness
+
+// derived data types:
+// object
+
+// print the data type:
+console.log(typeof "amit")
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
