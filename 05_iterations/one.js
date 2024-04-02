55df7d7d6568bb5c29c638663572154d3e72f331
Suhana Kaura <123460149+suhanakaura@users.noreply.github.com>
2024-04-02 09:42:04 +0000

    iteration

---
+++ 05_iterations/one.js
@@ -0,0 +1,36 @@
+// for
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+// for(let index = 0;index<=10;index++){
+//     console.log(index);
+// }
+
+// for(let i =1;i<=10;i++){
+//     for(let j=1;j<=10;j++){
+//         console.log(i ," * ",j," = ",i*j)
+//     }
+// }
+
+// let myarr = ["h","e","l","l","o"]
+// for(let i=0;i<myarr.length;i++){
+//     console.log(myarr[i])
+// }
+
+// break and continue
+// for(let i =0;i<=15;i++){
+//     if(i==6){
+//         console.log("6 detected")
+//         break;
+//     }
+//     console.log(`value is ${i}`)
+// }
+
+for(let i =0;i<=15;i++){
+    if(i==6){
+        console.log("6 detected")
+        continue;
+    }
+    console.log(`value is ${i}`)
+}
