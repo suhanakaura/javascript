e67865ebc81115ccdd97c040906df0cec2c590e1
Suhana Kaura <123460149+suhanakaura@users.noreply.github.com>
2024-04-03 07:53:42 +0000

    iteration

---
+++ 05_iterations/three.js
@@ -0,0 +1,51 @@
+// array specific loops
+// ["","",""]
+// [{},{},{}]
+
+// for of loop
+// const arr = [1,2,3,4,5]
+
+// for (const iterator of object) {
+    
+// }
+// for(const num of arr){
+//     console.log(num)
+// }
+
+// const str = "hello world!"
+// for(const s of str){
+//     if(s==' '){
+//         continue
+//     }
+//     console.log(s)
+// }
+
+
+// Maps
+const map = new Map()
+// setting values 
+map.set("IN","India")
+map.set("USA","United States of America")
+map.set("FR","France")
+map.set("IN","India")
+// unique values and will remain in same order
+// console.log(map)
+
+// looping in map
+// for(const key of map){
+//     console.log(key)
+// }
+
+// destructuring
+// for(const [key,value] of map){
+//     console.log(`key is ${key} and value is ${value}`)
+// }
+
+// not iteratable
+// const myObj = {
+//     "game1":"nfs",
+//     "game2":"spider man"
+// }
+// for(const [key,value] of myObj){
+//     console.log(`key is ${key} and value is ${value}`)
+// }
---
+++ 05_iterations/two.js
@@ -0,0 +1,36 @@
+// initialisation
+//  while (condition) {
+        // code
+        // condition's increment
+//  }
+
+// let index = 0;
+// while(index<=10){
+//     console.log(`Value of index is ${index}`)
+//     // index = index + 2
+//     index += 2
+// }
+
+
+// let myarr = ['h','e','l','l','o']
+// let i = 0;
+// while(i<myarr.length){
+//     console.log(myarr[i]);
+//     i++
+// }
+
+// do {
+    
+// } while (condition);
+
+// let score = 1
+// do{
+//     console.log(`score is ${score++}`)
+// }while(score<=10)
+
+// let score = 12
+// do{
+//     console.log(`score is ${score++}`)
+// }while(score<=10)
+// always runs one time for sure
+
